64d55b276d247ff59d5211334da7a4f37e30092d
rmm <mmei_ran@163.com>
2020-06-22 19:05:04 +0800

    dev no.28

---
+++ 28.实现-str-str.js
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=javascript
+ *
+ * [28] 实现 strStr()
+ *
+ * https://leetcode-cn.com/problems/implement-strstr/description/
+ *
+ * algorithms
+ * Easy (39.68%)
+ * Likes:    475
+ * Dislikes: 0
+ * Total Accepted:    184.5K
+ * Total Submissions: 464.8K
+ * Testcase Example:  '"hello"\n"ll"'
+ *
+ * 实现 strStr() 函数。
+ * 
+ * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置
+ * (从0开始)。如果不存在，则返回  -1。
+ * 
+ * 示例 1:
+ * 
+ * 输入: haystack = "hello", needle = "ll"
+ * 输出: 2
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: haystack = "aaaaa", needle = "bba"
+ * 输出: -1
+ * 
+ * 
+ * 说明:
+ * 
+ * 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+ * 
+ * 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+     // build next  假设模式串非空
+    let next = [-1];
+    let j = 1; //正在计算next[j]   比较next[j-1]与next[t]
+    let t = 0; 
+    while (j < needle.length) {
+        if (next[j-1] == next[t]) {
+            t++;
+            next[j] = t;
+            j++;
+        } else {
+            if (t > 0) {
+                t = 0;
+            } else {
+                next[j] = 0;
+                j++;
+            }
+        }
+    }
+};
+// @lc code=end
+
