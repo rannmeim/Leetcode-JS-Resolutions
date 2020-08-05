c8b7ba32407d93464eaf9b0c2118fd4d9e73f52a
rmm <mmei_ran@163.com>
2020-08-05 15:54:45 +0800

    add: 2020/08/05 daily no.337

---
+++ daily-question/337.打家劫舍-iii.js
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode.cn id=337 lang=javascript
+ *
+ * [337] 打家劫舍 III
+ *
+ * https://leetcode-cn.com/problems/house-robber-iii/description/
+ *
+ * algorithms
+ * Medium (57.23%)
+ * Likes:    490
+ * Dislikes: 0
+ * Total Accepted:    50.7K
+ * Total Submissions: 85.8K
+ * Testcase Example:  '[3,2,3,null,3,null,1]'
+ *
+ * 在上次打劫完一条街道之后和一圈房屋后，小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为“根”。
+ * 除了“根”之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。
+ * 如果两个直接相连的房子在同一天晚上被打劫，房屋将自动报警。
+ * 
+ * 计算在不触动警报的情况下，小偷一晚能够盗取的最高金额。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [3,2,3,null,3,null,1]
+ * 
+ * ⁠    3
+ * ⁠   / \
+ * ⁠  2   3
+ * ⁠   \   \ 
+ * ⁠    3   1
+ * 
+ * 输出: 7 
+ * 解释: 小偷一晚能够盗取的最高金额 = 3 + 3 + 1 = 7.
+ * 
+ * 示例 2:
+ * 
+ * 输入: [3,4,5,1,3,null,1]
+ * 
+ * 3
+ * ⁠   / \
+ * ⁠  4   5
+ * ⁠ / \   \ 
+ * ⁠1   3   1
+ * 
+ * 输出: 9
+ * 解释: 小偷一晚能够盗取的最高金额 = 4 + 5 = 9.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var rob = function (root) {
+    /**
+     *
+     *
+     * @param {TreeNode} target 当前要去的人家
+     * @param {Boolean} rootRobbed 根人家是否被抢了
+     * @return {Number} 当前人家及子人家 在根人家是否被抢的情况下 最多能抢到多少钱
+     */
+    function robOneHouse(target, rootRobbed) {
+        if (!target) {
+            return 0
+        }
+        let notRobTarget = robOneHouse(target.left, false) + robOneHouse(target.right, false)
+        if (rootRobbed) {
+            return notRobTarget
+        } else {
+            let robTarget = robOneHouse(target.left, true) + robOneHouse(target.right, true) + target.val
+            return Math.max(robTarget, notRobTarget)
+        }
+    }
+    let result = robOneHouse(root, false)
+    return result
+
+};
+// @lc code=end
+
+
+// @after-stub-for-debug-begin
+module.exports = rob;
+// @after-stub-for-debug-end
